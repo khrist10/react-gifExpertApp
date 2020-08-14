d6e306fc83843ce1b9a36fc85c3f5db84a60d109
Christian Flores <ChristianFlores.arriagada@gmail.com>
2020-08-14 00:15:53 -0400

    docs agregado

---
+++ docs/precache-manifest.bf0f9f3e83be704b4a03d9ace9541e15.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b7753c85a42d447a554c69a8afb50f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f1c394d8e272270c25d4",
+    "url": "/static/css/main.d1780445.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1c394d8e272270c25d4",
+    "url": "/static/js/main.de5ced94.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
